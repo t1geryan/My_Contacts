c9f84cff46f699e24c525ae6cb7f7a1f681cdaf4
t1geryan <sgta1980@gmail.com>
2023-02-08 23:02:41 +0300

    draft - contact list fragment, no functionality

---
+++ app/src/main/java/com/example/mycontacts/view/ContactListFragment.kt
@@ -0,0 +1,38 @@
+package com.example.mycontacts.view
+
+import android.os.Bundle
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mycontacts.databinding.FragmentContactListBinding
+import com.example.mycontacts.model.Repository
+import com.example.mycontacts.view.adapter.ContactsAdapter
+
+class ContactListFragment : Fragment() {
+
+    private lateinit var binding: FragmentContactListBinding
+    private lateinit var adapter: ContactsAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentContactListBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = ContactsAdapter()
+        binding.recyclerView.adapter = adapter
+        adapter.contacts = Repository.contacts
+    }
+
+
+}
