a9ff68f75dac78bd3ea14762f186f4ffdda9eb6d
t1geryan <sgta1980@gmail.com>
2023-03-03 14:54:22 +0300

    change model - add App class

--- app/src/main/java/com/example/mycontacts/view/ContactListFragment.kt
+++ app/src/main/java/com/example/mycontacts/view/ContactListFragment.kt
@@ -6,20 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.mycontacts.App
 import com.example.mycontacts.R
 import com.example.mycontacts.databinding.FragmentContactListBinding
 import com.example.mycontacts.model.Contact
 import com.example.mycontacts.model.OnContactChangeListener
 import com.example.mycontacts.model.ContactListLocalService
+import com.example.mycontacts.model.ContactListRepository
 import com.example.mycontacts.view.utils.*
 
 class ContactListFragment : Fragment(), HasCustomActionToolbar, HasCustomTitleToolbar {
 
     private lateinit var binding: FragmentContactListBinding
     private lateinit var adapter: ContactsAdapter
-    private val listener = ContactListLocalService.OnContactListChangeListener {
-        adapter.contacts = it
-    }
+
+    private lateinit var contactListLocalService: ContactListLocalService
+
+    private lateinit var listener : ContactListRepository.OnContactListChangeListener
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -31,30 +34,38 @@ class ContactListFragment : Fragment(), HasCustomActionToolbar, HasCustomTitleTo
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        contactListLocalService = (context?.applicationContext as App).contactListLocalService
+
+        listener = contactListLocalService.OnContactListChangeListener {
+            adapter.contacts = it
+        }
+
         adapter = ContactsAdapter(object : OnContactChangeListener {
             override fun onDetails(contact: Contact) {
                 InputContactDialogFragment.newInstance(contact.name, contact.number).show(parentFragmentManager, InputContactDialogFragment.TAG)
             }
 
             override fun onDeleteContact(contact: Contact) {
-                ContactListLocalService.deleteContact(contact)
+                contactListLocalService.deleteContact(contact)
             }
 
             override fun onChangeFavoriteStatus(contact: Contact) {
-                ContactListLocalService.changeContactFavoriteStatus(contact)
+                contactListLocalService.changeContactFavoriteStatus(contact)
             }
 
         })
+
         binding.recyclerView.layoutManager = GridLayoutManager(requireContext(),
             RecyclerViewUtility.calculateNoOfColumns(requireContext(), 136F))
         binding.recyclerView.adapter = adapter
-        adapter.contacts = ContactListLocalService.contacts
-        ContactListLocalService.addListener(listener)
+        adapter.contacts = contactListLocalService.contacts
+        contactListLocalService.addListener(listener)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        ContactListLocalService.removeListener(listener)
+        contactListLocalService.removeListener(listener)
     }
 
     override fun getCustomAction(): Action {
@@ -62,7 +73,7 @@ class ContactListFragment : Fragment(), HasCustomActionToolbar, HasCustomTitleTo
             InputContactDialogFragment.newInstance().show(parentFragmentManager, InputContactDialogFragment.TAG)
         }
         InputContactDialogFragment.setupResultListener(parentFragmentManager, viewLifecycleOwner) { contact ->
-            ContactListLocalService.addContact(contact)
+            contactListLocalService.addContact(contact)
         }
 
         return Action(R.drawable.ic_add_contact_white, R.string.add_contact, onAction)
