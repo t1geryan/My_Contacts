f14186b18bff87893221e6a5defca8303b699a30
t1geryan <sgta1980@gmail.com>
2023-03-20 23:07:43 +0300

    delete empty realizations from OnContactChangeListener.kt interface

--- app/src/main/java/com/example/mycontacts/domain/model/OnContactChangeListener.kt
+++ app/src/main/java/com/example/mycontacts/domain/model/OnContactChangeListener.kt
@@ -1,24 +1,11 @@
 package com.example.mycontacts.domain.model
 
-/*
-    due to the presence of empty functions,
-    the implementation will be able to determine only those listeners
-    that are needed for a particular screen
- */
 interface OnContactChangeListener {
-    fun onDelete(contact: Contact) {
+    fun onDelete(contact: Contact)
 
-    }
+    fun onChangeData(contact: Contact)
 
-    fun onChangeData(contact: Contact) {
+    fun onChangeFavoriteStatus(contact: Contact)
 
-    }
-
-    fun onChangeFavoriteStatus(contact: Contact) {
-
-    }
-
-    fun onCall(contact: Contact) {
-
-    }
+    fun onCall(contact: Contact)
 }
